0a2ac7bf5a38436a837403300e503010241b6a73
Aquasafe <aquasaferoworks@gmail.com>
2026-05-15 10:56:59 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAs-placeholder",
-  authDomain: "aquasafe-hub.firebaseapp.com",
-  projectId: "aquasafe-hub",
-  storageBucket: "aquasafe-hub.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDA3n8YcwHUKjUYrvIgXSidsh8lAHuNezc",
+  authDomain: "studio-5939212922-a8c34.firebaseapp.com",
+  projectId: "studio-5939212922-a8c34",
+  storageBucket: "studio-5939212922-a8c34.firebasestorage.app",
+  messagingSenderId: "255557828717",
+  appId: "1:255557828717:web:024aaebc7342ff2ecb7f64"
 };
